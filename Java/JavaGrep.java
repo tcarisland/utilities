11935d9517bba628b3fe8a5139ddcacabdf15845
tcarisland <thor.arisland@aurorainnovation.com>
2018-06-15 10:20:11 +0200

    JavaGrep updated

--- Java/JavaGrep.java
+++ Java/JavaGrep.java
@@ -4,38 +4,51 @@ import java.io.*;
 
 public class JavaGrep {
 
+    private String inputFileName;
+    
     public static void main(String args[]) {
-	if(args.length > 0) {
-	    doJavaGrep(args[0]);
+	if(args.length == 1) {
+	    JavaGrep jg = new JavaGrep(args[0]);
+	    jg.doJavaGrep();
+	} else if (args.length > 0) {
+	    JavaGrep jg = new JavaGrep(args[0]);
+	    jg.doJavaGrep(args[1]);
 	}
     }
 
-    public static void doJavaGrep(String inputFileName) {
-	String grepLine = "([0-9\\.:].*?)";
-	doJavaGrep(inputFileName, grepLine);
+    public JavaGrep(String inputFileName) {
+	this.inputFileName = inputFileName;
     }
     
-    public static void doJavaGrep(String inputFileName, String grep) {
+    public void doJavaGrep() {
+	String grepLine = "([0-9\\.:].*?)";
+	doJavaGrep(grepLine);
+    }
+    
+    public void doJavaGrep(String grep) {
 	try {
+	    System.out.println("GREPLINE: " + grep);
 	    File inputFile = new File(inputFileName);
 	    Scanner in = new Scanner(inputFile);
 	    String newFileName = inputFile.getPath();
 	    if(newFileName.contains(".")) {
-		String filePrefix = newFileName.substring(0, newFileName.length() - 4);
-		String fileSuffix = newFileName.substring(newFileName.length() - 4);
+		String filePrefix = newFileName.substring(0, newFileName.length() - (newFileName.length() - newFileName.indexOf('.')));
+		String fileSuffix = newFileName.substring(newFileName.length() - (newFileName.length() - newFileName.indexOf('.')));
 		newFileName = filePrefix + "_NEW" + fileSuffix;
 	    } else {
 		newFileName = newFileName + "_NEW.txt";
 	    }
 	    
 	    PrintWriter out = new PrintWriter(new File(newFileName));
-	    System.out.println("GOING INTO LOOP");
+
 	    String inputLine, outputLine = "";
 	    while(in.hasNextLine()) {
 		inputLine = in.nextLine();
 		outputLine = inputLine.replaceAll(grep, "");
+		System.out.println(outputLine);
 		out.println(outputLine);
 	    }
+	    out.close();
 	} catch (Exception e) {
 	    System.out.println("EXCEPTION!");
 	    e.printStackTrace();
